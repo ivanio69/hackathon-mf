0e6527b56918d781c25d2e610194660e21ccfd07
/lifecats <lifecatsdev@gmail.com>
2022-01-06 12:41:53 +0300

    fuck me

--- _app.js
+++ _app.js
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
 import { getFromLocalStorage } from "./_lib";
-import NavBar from "./_nav";
+import NavBar from "./pages/_nav";
 
 function MyApp({ Component, pageProps }) {
   return (
