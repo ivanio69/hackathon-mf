c20f6afbb917cb1b134e034791b2f9805052c29b
/lifecats <lifecatsdev@gmail.com>
2022-01-06 12:46:54 +0300

    AAAAAHHH IM CUMMING

--- _nav.js
+++ _nav.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import styles from "../styles/nav.module.css";
+import styles from "./styles/nav.module.css";
 export default function NavBar() {
   return (
     <>
