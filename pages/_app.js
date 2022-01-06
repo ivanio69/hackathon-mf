a518945107b7f27751dba6523962e8e26bc58778
/lifecats <lifecatsdev@gmail.com>
2022-01-06 12:44:36 +0300

    oh yeah fucking

---
+++ pages/_app.js
@@ -0,0 +1,20 @@
+import "../styles/globals.css";
+import { getFromLocalStorage } from "../_lib";
+import NavBar from "./_nav";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <NavBar />
+    </>
+  );
+}
+
+export default MyApp;
+if (process.browser)
+  if (getFromLocalStorage("theme") === "dark") {
+    document.body.classList.add("dark");
+  } else {
+    document.body.classList.add("light");
+  }
