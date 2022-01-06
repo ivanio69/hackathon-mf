ed132215dedc57549d2d777d3df085795dfa4800
/lifecats <lifecatsdev@gmail.com>
2022-01-06 12:39:33 +0300

    fixes fi=xes fixes

--- pages/_nav.js
+++ pages/_nav.js
@@ -3,22 +3,34 @@ import Link from "next/link";
 import styles from "../styles/nav.module.css";
 export default function NavBar() {
   return (
-    <div className={styles.footerbox}>
-      <Link href="/tasks">
-        <Image className={styles.svg} width={36} height={60} src="/svg1.svg" />
-      </Link>{" "}
-      <Link href="/">
-        <Image
-          stroke="#000"
-          className={styles.svg}
-          width={36}
-          height={60}
-          src="/svg3.svg"
-        />
-      </Link>
-      <Link href="/profile">
-        <Image className={styles.svg} width={36} height={60} src="/svg2.svg" />{" "}
-      </Link>{" "}
-    </div>
+    <>
+      <div className={styles.footerbox}>
+        <Link href="/tasks">
+          <Image
+            className={styles.svg}
+            width={36}
+            height={60}
+            src="/svg1.svg"
+          />
+        </Link>
+        <Link href="/">
+          <Image
+            stroke="#000"
+            className={styles.svg}
+            width={36}
+            height={60}
+            src="/svg3.svg"
+          />
+        </Link>
+        <Link href="/profile">
+          <Image
+            className={styles.svg}
+            width={36}
+            height={60}
+            src="/svg2.svg"
+          />{" "}
+        </Link>
+      </div>
+    </>
   );
 }
