61ce4f4b007cdc21685ef973d02a65422c82b17e
/lifecats <lifecatsdev@gmail.com>
2022-01-12 11:52:28 -0800

    add icons

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import "../styles/globals.css";
 import { getFromLocalStorage } from "../_lib";
 import NavBar from "../_nav";
-
+import Head from "next/head";
 process.browser
   ? (window.onload = () => {
       // document.body.style.animation = "appear 0.3s";
@@ -14,6 +14,23 @@ process.browser
 function MyApp({ Component, pageProps }) {
   return (
     <div>
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          href="/favicon-16x16.png"
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+        />
+        <link
+          href="/favicon-32x32.png"
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+        />
+        <link rel="apple-touch-icon" href="/ios/192x192.png"></link>
+        <meta name="theme-color" content="#317EFB" />
+      </Head>
       <Component {...pageProps} />
       <NavBar />
     </div>
